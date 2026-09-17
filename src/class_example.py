ba3200d2490e542931e9a1857ab08f329b4b8200
Vallejos, Jorge <jorge.vallejos@experian.com>
2026-09-17 11:06:02 -0500

    class example

---
+++ src/class_example.py
@@ -0,0 +1,24 @@
+# class definition
+class Guitar:
+    def __init__(self, brand, model, price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+
+    def display_info(self):
+        print(f"Brand: {self.brand}, Model: {self.model}, Price: ${self.price}")
+
+    #Methods
+    def play(self):
+        print(f"{self.brand} {self.model} is playing a beautiful melody.")
+
+#Object instantiation
+guitar1 = Guitar("Schecter", "Hell Raiser", 1000)
+guitar2 = Guitar("Alhambra", "Semi Concert", 500)
+
+#Calling methods    
+guitar1.play()
+guitar2.play()
+
+print(guitar1.brand)
+print(guitar2.model)
